30cf7f190083446bc9349496b2ce3ba771a659be
robertcramer <robertbcramer@icloud.com>
2016-01-22 13:20:07 -0500

    built basic controller

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -1,5 +1,11 @@
 class InvitesController < ApplicationController
   def create
+    mandrill = Mandrill:API.new(MANDRILL_API)
 
+    message = {
+
+    }
+
+    mandrill.messages.send message
   end
 end
