4fbe90ec51cb321fe547bce1bc034734d28a1a41
robertcramer <robertbcramer@icloud.com>
2016-01-22 13:29:42 -0500

    updated create method

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -3,7 +3,10 @@ class InvitesController < ApplicationController
     mandrill = Mandrill:API.new(MANDRILL_API)
 
     message = {
-
+        text: params[:text],
+        subject: params[:subject],
+        from_email: "andrewkfaircloth@gmail.com",
+        to: [{email: "robertbcramer@icloud.com"}]
     }
 
     mandrill.messages.send message
