e10de17425315b5420ad9946c7eebf1b0da1a712
robertcramer <robertbcramer@icloud.com>
2016-01-22 15:05:19 -0500

    string interpolation in subject

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -2,9 +2,11 @@ class InvitesController < ApplicationController
   def create
     mandrill = Mandrill::API.new ENV["MANDRILL_API_KEY"]
 
+    name = params[:name]
+
     message = {
         text: params[:text],
-        subject: params[:subject],
+        subject: "#{name} invites you to subscribe!",
         from_email: "andrewkfaircloth@gmail.com",
         to: [{email: "robertbcramer@icloud.com"}]
     }
