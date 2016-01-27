b11add2d1f0f0002211bd3a3dcf6d6f89ae6d3c7
robertcramer <robertbcramer@icloud.com>
2016-01-27 09:21:50 -0500

    created path to template

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -7,10 +7,12 @@ class InvitesController < ApplicationController
     email_string = params[:list]
     email_array = email_string.split(',')
 
+    invites_template = to_invites(binding)
+
     email_array.each do |send|
 
     message = {
-        text: params[:text],
+        html: invites_template,
         subject: "#{name} invites you to subscribe!",
         from_email: email,
         to: [{email: send}]
@@ -21,5 +23,13 @@ class InvitesController < ApplicationController
     end
     render json: { message: "Thanks for sharing!" }, status: :ok
 
+  end
+
+  private
+  def to_invites(controller_binding)
+    template_path = Rails.root.join("app", "views", "invites", "invites_template.html.erb")
+    html = File.open(template_path).read
+    template = ERB.new(html)
+    template.result(controller_binding)
   end
 end
